3248b3d6add52809b9fc5f65b3742c4faf627704
Jack Kelly <jack-list@xlk.org.uk>
2017-06-22 10:00:43 +0100

    adding HTML docs

---
+++ docs/navtree.js
@@ -0,0 +1,358 @@
+var NAVTREE =
+[
+  [ "Disaggregate", "index.html", [
+    [ "Related Pages", "pages.html", [
+      [ "Deprecated List", "deprecated.html", null ]
+    ] ],
+    [ "Class List", "annotated.html", [
+      [ "AggregateData", "class_aggregate_data.html", null ],
+      [ "AggregateSample", "struct_aggregate_sample.html", null ],
+      [ "Array< T >", "class_array.html", null ],
+      [ "Device", "class_device.html", null ],
+      [ "PowerStateGraph::Disag_edge_writer", "struct_power_state_graph_1_1_disag__edge__writer.html", null ],
+      [ "PowerStateGraph::Disag_vertex_writer", "struct_power_state_graph_1_1_disag__vertex__writer.html", null ],
+      [ "PowerStateGraph::DisagVertex", "struct_power_state_graph_1_1_disag_vertex.html", null ],
+      [ "PowerStateGraph::Fingerprint", "struct_power_state_graph_1_1_fingerprint.html", null ],
+      [ "AggregateData::FoundSpike", "struct_aggregate_data_1_1_found_spike.html", null ],
+      [ "Histogram", "class_histogram.html", null ],
+      [ "Device::IndexAndDelta", "struct_device_1_1_index_and_delta.html", null ],
+      [ "PowerStateGraph::LikelihoodAndVertex", "struct_power_state_graph_1_1_likelihood_and_vertex.html", null ],
+      [ "GNUplot::PlotData", "struct_g_n_uplot_1_1_plot_data.html", null ],
+      [ "GNUplot::PlotVars", "struct_g_n_uplot_1_1_plot_vars.html", null ],
+      [ "PowerStateGraph::PowerStateEdge", "struct_power_state_graph_1_1_power_state_edge.html", null ],
+      [ "PowerStateGraph", "class_power_state_graph.html", null ],
+      [ "PowerStateSequence", "class_power_state_sequence.html", null ],
+      [ "PowerStateSequenceItem", "struct_power_state_sequence_item.html", null ],
+      [ "PowerStateGraph::PowerStateVertex", "struct_power_state_graph_1_1_power_state_vertex.html", null ],
+      [ "PowerStateGraph::PSG_edge_writer", "struct_power_state_graph_1_1_p_s_g__edge__writer.html", null ],
+      [ "PowerStateGraph::PSG_vertex_writer", "struct_power_state_graph_1_1_p_s_g__vertex__writer.html", null ],
+      [ "Signature", "class_signature.html", null ],
+      [ "Signature::Spike", "struct_signature_1_1_spike.html", null ],
+      [ "Statistic< T >", "struct_statistic.html", null ],
+      [ "PowerStateGraph::TimeAndPower", "struct_power_state_graph_1_1_time_and_power.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "AggregateSample", "struct_aggregate_sample.html", null ],
+      [ "Array< T >", "class_array.html", null ],
+      [ "Array< AggregateSample >", "class_array.html", [
+        [ "AggregateData", "class_aggregate_data.html", null ]
+      ] ],
+      [ "Array< Histogram_t >", "class_array.html", [
+        [ "Histogram", "class_histogram.html", null ]
+      ] ],
+      [ "Array< Sample_t >", "class_array.html", [
+        [ "Signature", "class_signature.html", null ]
+      ] ],
+      [ "Device", "class_device.html", null ],
+      [ "PowerStateGraph::Disag_edge_writer", "struct_power_state_graph_1_1_disag__edge__writer.html", null ],
+      [ "PowerStateGraph::Disag_vertex_writer", "struct_power_state_graph_1_1_disag__vertex__writer.html", null ],
+      [ "PowerStateGraph::DisagVertex", "struct_power_state_graph_1_1_disag_vertex.html", null ],
+      [ "PowerStateGraph::Fingerprint", "struct_power_state_graph_1_1_fingerprint.html", null ],
+      [ "AggregateData::FoundSpike", "struct_aggregate_data_1_1_found_spike.html", null ],
+      [ "Device::IndexAndDelta", "struct_device_1_1_index_and_delta.html", null ],
+      [ "PowerStateGraph::LikelihoodAndVertex", "struct_power_state_graph_1_1_likelihood_and_vertex.html", null ],
+      [ "GNUplot::PlotData", "struct_g_n_uplot_1_1_plot_data.html", null ],
+      [ "GNUplot::PlotVars", "struct_g_n_uplot_1_1_plot_vars.html", null ],
+      [ "PowerStateGraph::PowerStateEdge", "struct_power_state_graph_1_1_power_state_edge.html", null ],
+      [ "PowerStateGraph", "class_power_state_graph.html", null ],
+      [ "PowerStateSequence", "class_power_state_sequence.html", null ],
+      [ "PowerStateSequenceItem", "struct_power_state_sequence_item.html", null ],
+      [ "PowerStateGraph::PowerStateVertex", "struct_power_state_graph_1_1_power_state_vertex.html", null ],
+      [ "PowerStateGraph::PSG_edge_writer", "struct_power_state_graph_1_1_p_s_g__edge__writer.html", null ],
+      [ "PowerStateGraph::PSG_vertex_writer", "struct_power_state_graph_1_1_p_s_g__vertex__writer.html", null ],
+      [ "Signature::Spike", "struct_signature_1_1_spike.html", null ],
+      [ "Statistic< T >", "struct_statistic.html", null ],
+      [ "PowerStateGraph::TimeAndPower", "struct_power_state_graph_1_1_time_and_power.html", null ],
+      [ "Histogram_t *", "class_histogram__t_01_5.html", null ],
+      [ "Sample_t *", "class_sample__t_01_5.html", null ],
+      [ "size_t", "classsize__t.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "GNUplot", "namespace_g_n_uplot.html", null ],
+      [ "Utils", "namespace_utils.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/AggregateData.cpp", "_aggregate_data_8cpp.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/AggregateData.d", "_aggregate_data_8d.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/AggregateData.h", "_aggregate_data_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Array.h", "_array_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Common.h", "_common_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Device.cpp", "_device_8cpp.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Device.d", "_device_8d.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Device.h", "_device_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/GNUplot.cpp", "_g_n_uplot_8cpp.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/GNUplot.d", "_g_n_uplot_8d.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/GNUplot.h", "_g_n_uplot_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Histogram.cpp", "_histogram_8cpp.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Histogram.h", "_histogram_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Main.cpp", "_main_8cpp.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Main.d", "_main_8d.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/PowerStateGraph.cpp", "_power_state_graph_8cpp.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/PowerStateGraph.d", "_power_state_graph_8d.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/PowerStateGraph.h", "_power_state_graph_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/PowerStateSequence.cpp", "_power_state_sequence_8cpp.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/PowerStateSequence.d", "_power_state_sequence_8d.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/PowerStateSequence.h", "_power_state_sequence_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Signature.cpp", "_signature_8cpp.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Signature.d", "_signature_8d.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Signature.h", "_signature_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Statistic.h", "_statistic_8h.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Utils.cpp", "_utils_8cpp.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Utils.d", "_utils_8d.html", null ],
+      [ "/mnt/sshfs/imperialroot/homes/dk3810/workingcopies/disaggregate/src/Utils.h", "_utils_8h.html", null ]
+    ] ],
+    [ "Directories", "dirs.html", [
+      [ "src", "dir_eef90bc061bc2f5fb6481af7427deb52.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
